2675f2585b0663ba938f206220e41bc80489958a
R3tr0 <toby@soegaards.dk>
2024-10-04 00:41:26 +0200

    Slash command added (NOT WORKING ATM)

--- root/main.js
+++ root/main.js
@@ -1,5 +1,8 @@
-const {Client, GatewayIntentBits} = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const {Client, GatewayIntentBits, Events, Collection} = require('discord.js');
 const { TOKEN } = require('./config.json');
+;
 
 const bot = new Client({ intents: [
   GatewayIntentBits.Guilds,
@@ -7,23 +10,51 @@ const bot = new Client({ intents: [
 ]
 });
 
-bot.on('ready', () => {
-  console.log(`Logged in as ${bot.user.tag}!`);
-});
+bot.commands = new Collection();
 
-bot.on('interactionCreate', async interaction => {
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			bot.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+bot.on(Events.InteractionCreate, async interaction => {
   if (!interaction.isChatInputCommand()) return;
 
-  if (interaction.commandName == 'ping') {
-    await interaction.reply('Pong!');
+  const command = interaction.bot.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command found matching ${interaction.commandName}`);
+    return;
+  }
+
+  try {
+      await command.execute(interaction);
+  } catch (error) {
+      console.error(error);
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true});
+    } else {
+        await interaction.reply({ conten: 'There was an error while executing this command!', ephemeral: true});
+    }
   }
 });
 
-const test = [
-    {
-        name: 'ping',
-        description: 'Pong!',
-    },
-];
+bot.once(Events.ClientReady, readybot => {
+  console.log(`Logged in as ${readybot.user.tag}!`);
+});
+
 
 bot.login(TOKEN);
